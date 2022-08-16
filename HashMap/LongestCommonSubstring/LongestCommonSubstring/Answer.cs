c666f39871bfae86c24621d289a430bb3fdb9e14
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2022-08-17 08:00:10 +1000

    naive implementation of Long Common Substring problem

---
+++ HashMap/LongestCommonSubstring/LongestCommonSubstring/Answer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongestCommonSubstring
+{
+    internal class Answer
+    {
+        public int Length { get; set; }
+        public int I { get; set; }
+        public int J { get; set; }
+    }
+}
---
+++ HashMap/LongestCommonSubstring/LongestCommonSubstring/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LongestCommonSubstring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            var s = input[0];
+            var t = input[1];
+            var answer = new Answer();
+            for(int i= 0; i < input.Length; i++)
+            {
+                for(int j = 0; j < input.Length; j++)
+                {
+                    for(int len = 0; i + len <= s.Length && j + len <= t.Length; len++) // C => size of the smallest input
+                    {
+                        if(len > answer.Length && s.Substring(i, i + len).Equals(t.Substring(j, j + len))) //
+                        {
+                            answer.Length = len;
+                            answer.I = i;
+                            answer.J = j;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"{answer.I} {answer.J} {answer.Length}");
+        }
+    }
+}
