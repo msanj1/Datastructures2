fbd2e7dc0cf22c688d021358bf64f64c7adeab54
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2022-09-14 08:37:02 +1000

    added Binary Tree Implementations

---
+++ Binary Trees/Traversals/Traversals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Traversals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfVertices = int.Parse(Console.ReadLine());
+            var tree = new BinaryTree(numberOfVertices);
+            for(int i=0; i < numberOfVertices; i++)
+            {
+                var treeInputs = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
+                tree.Add(treeInputs[0], treeInputs[1], treeInputs[2]);
+            }
+
+            Console.WriteLine(String.Join(" ",tree.TraverseInOrder().Select(x=>x.Key)));
+            Console.WriteLine(String.Join(" ", tree.TraversePreOrder().Select(x => x.Key)));
+            Console.WriteLine(String.Join(" ", tree.TraversePostOrder().Select(x => x.Key)));
+        }
+    }
+}
